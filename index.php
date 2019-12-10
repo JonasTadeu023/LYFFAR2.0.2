ccf6fdc653cecd1b013687737f592a086ca38cb2
Jonas Tadeu Saldanha Junior <51368425+JonasTadeu023@users.noreply.github.com>
2019-12-09 22:38:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <!-- link de icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>LYFFAR</title>
+    <style>   
+        .logo_img {width: 65px; margin-top: -10px;}
+        .perf_img {width: 40px; margin-top: 13px; margin-right: 0px}
+    </style>
+</head>
+<body>
+    <!--Side bar fodahhhh-->
+    <ul id="slide-out" class="sidenav sidenav-fixed grey darken-4 z-depth-5">
+        <a href="#!"><img src="imgs/logo1.png" class="logo_img" style="margin-top:10px;margin-left:30px;"></a>
+        <li class="tab"><a href="templates/cadastro.php"><i class="white-text material-icons">person_add</i><p class="white-text"> Criar conta</p></a></li>
+        <li class="tab "><a href="templates/login.php"><i class="white-text material-icons">people</i><p class="white-text">Entrar em sua conta</p></a></li>
+        <li class="tab "><a href="linha.php"><i class="white-text material-icons">book</i><p class="white-text">Linha do tempo</p></a></li>
+        <li class="tab"><a href="postagens.php"><i class="white-text material-icons">photo_library</i><p class="white-text">Lembranças do IFFAR-FW</p></a></li>
+    </ul>
+        
+</body>
+</html>
